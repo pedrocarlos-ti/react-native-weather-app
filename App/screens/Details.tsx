3efd47a04f30519c8a49a2205e96cbf98b944245
Pedro Santos <pedrocarlos.ti@gmail.com>
2020-09-02 22:09:01 -0300

    add: Search and Details components

--- App/screens/Details.tsx
+++ App/screens/Details.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import { ActivityIndicator, ScrollView } from 'react-native';
-import { weatherApi, Weather } from '../util/weatherApi';
+import { ActivityIndicator, ScrollView, View } from 'react-native';
+import { format } from 'date-fns';
 
+import { weatherApi, Weather } from '../util/weatherApi';
 import { Container } from '../components/Container';
 import { WeatherIcon } from '../components/WeatherIcon';
 import { BasicRow } from '../components/List';
-import { H1, H2 } from '../components/Text';
+import { H1, H2, P } from '../components/Text';
+
+const groupForecastByday = (list) => {
+  const data = {};
+
+  list.forEach((item) => {
+    const [day] = item.dt_txt.split(' ');
+    if (data[day]) {
+      if (data[day].temp_max < item.main_max) {
+        data[day].temp_max = item.main.temp_max;
+      }
+
+      if (data[day].temp_min > item.main_min) {
+        data[day].temp_min = item.main.temp_min;
+      }
+    } else {
+      data[day] = {
+        temp_min: item.main.temp_min,
+        temp_max: item.main.temp_max
+      };
+    }
+
+    data[day] = {
+      temp_min: item.main.temp_min,
+      temp_max: item.main.temp_max
+    };
+  });
+
+  const formattedList = Object.keys(data).map((key) => ({
+    day: key,
+    ...data[key]
+  }));
+
+  return formattedList;
+};
 
 export default class Details extends React.Component {
   state = {
     currentWeather: {},
-    loadingCurrentWeather: true
+    loadingCurrentWeather: true,
+    forecast: [],
+    loadingForecast: true
   };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition((position) => {
       // console.log(position.coords);
       this.getCurrentWeather({ coords: position.coords });
-      // this.getForecast({ coords: position.coords });
+      this.getForecast({ coords: position.coords });
     });
-
-    const zipcode = 54160546;
   }
 
   getCurrentWeather = ({ zipcode, coords }: Weather) =>
@@ -36,7 +71,12 @@ export default class Details extends React.Component {
 
   getForecast = ({ zipcode, coords }: Weather) =>
     weatherApi('/forecast', { zipcode, coords })
-      .then((response) => {})
+      .then((response) => {
+        this.setState({
+          loadingForecast: false,
+          forecast: groupForecastByday(response.list)
+        });
+      })
       .catch((err) => console.log(err));
 
   render() {
@@ -50,8 +90,6 @@ export default class Details extends React.Component {
 
     const { weather, main } = this.state.currentWeather;
 
-    console.log(main);
-
     return (
       <Container>
         <ScrollView>
@@ -64,6 +102,23 @@ export default class Details extends React.Component {
             <H2>{`Mínimo: ${Math.round(main.temp_min)}º`}</H2>
             <H2>{`Máximo: ${Math.round(main.temp_max)}º`}</H2>
           </BasicRow>
+
+          <View style={{ paddingHorizontal: 10, marginTop: 20 }}>
+            {this.state.forecast.map((day) => (
+              <BasicRow
+                key={day.day}
+                style={{ justifyContent: 'space-between' }}
+              >
+                <P>{format(new Date(day.day), 'dd/MM/yyyy')}</P>
+                <View style={{ flexDirection: 'row' }}>
+                  <P style={{ fontWeight: '700', marginRight: 10 }}>
+                    {Math.round(day.temp_max)}
+                  </P>
+                  <P>{Math.round(day.temp_min)}</P>
+                </View>
+              </BasicRow>
+            ))}
+          </View>
         </ScrollView>
       </Container>
     );
--- App/screens/Search.tsx
+++ App/screens/Search.tsx
@@ -1,1 +1,58 @@
-export const Search = () => null;
+import React, { Component } from 'react';
+import { Text, View, FlatList } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import { SearchBar } from '../components/SearchBar';
+import { SearchItem } from '../components/List';
+
+class Search extends Component {
+  state = {
+    query: ''
+  };
+
+  render() {
+    return (
+      <FlatList
+        data={[
+          { id: 1, name: 'Franklin' },
+          { id: 2, name: 'Mountain View' }
+        ]}
+        renderItem={({ item }) => (
+          <SearchItem
+            name={item.name}
+            onPress={() => this.props.navigation.navigate('Details')}
+          />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        ListHeaderComponent={
+          <View>
+            <StatusBar style="dark" />
+            <SearchBar
+              searchButtonEnabled={this.state.query.length >= 5}
+              onChangeText={(query) => this.setState({ query })}
+              onSearch={() => {
+                this.props.navigation.navigate('Details', {
+                  zipcode: this.state.query
+                });
+              }}
+              placeholder="Address"
+            />
+            <Text
+              style={{
+                marginHorizontal: 10,
+                fontSize: 16,
+                color: '#aaa',
+                marginTop: 10,
+                marginBottom: 5
+              }}
+            >
+              Recents
+            </Text>
+          </View>
+        }
+      />
+    );
+  }
+}
+
+export default Search;
